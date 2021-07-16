4b773c629c8975ae0d89f7e4d4e13fda652a89a6
Chethan Bhandarkar B <b.chethanb91@gmail.com>
2021-07-16 15:15:59 +0530

    IMBd

--- app/src/main/java/com/chethanbhandarkar/imbd/MainActivity.kt
+++ app/src/main/java/com/chethanbhandarkar/imbd/MainActivity.kt
@@ -26,6 +26,8 @@ class MainActivity : AppCompatActivity() {
 				R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
 			)
 		)
+
+		//new line
 		setupActionBarWithNavController(navController, appBarConfiguration)
 		navView.setupWithNavController(navController)
 	}
